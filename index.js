626d7f3899a95fce3c8ee11d65cb1504b67810cc
neoja <wjuma19dev@yahoo.com>
2022-05-11 18:49:42 -0400

    Config button to acces dv tools

--- index.js
+++ index.js
@@ -3,22 +3,30 @@ const electron = require("electron");
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
-let newWindow;
+let addTodoWindow;
 
 app.on("ready", () => {
+  // Main Window Create
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
 
+  // NOTE: when app closed force other windows to close
+  mainWindow.on("closed", () => app.quit());
+
+  // Main Menu Create
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
 });
 
 const createNewWindow = () => {
-  newWindow = new BrowserWindow({
+  addTodoWindow = new BrowserWindow({
     width: 300,
     height: 200,
     title: "Add new todo",
+    resizable: true,
+    autoHideMenuBar: true,
   });
+  addTodoWindow.loadURL(`file://${__dirname}/add-todos.html`);
 };
 
 const menuTemplate = [
@@ -52,4 +60,26 @@ const menuTemplate = [
 // En MAC agrega un nuevo elemento al inicio del menu
 if (process.platform === "darwin") {
   menuTemplate.unshift({});
+}
+
+// Open Dev Tools in Dev Mode
+if (process.env.NODE_ENV !== "production") {
+  menuTemplate.push({
+    label: "View",
+    submenu: [
+      {
+        label: "More tools",
+        submenu: [
+          {
+            label: "Developer tools",
+            accelerator:
+              process.platform === "darwin" ? "Command+Alt+I" : "F12",
+            click(item, focusedWindow) {
+              focusedWindow.toggleDevTools();
+            },
+          },
+        ],
+      },
+    ],
+  });
 }
