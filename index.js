bf5e1d115e46e849d04b36363a95643357cb29a9
neoja <wjuma19dev@yahoo.com>
2022-05-11 17:54:50 -0400

    Agregado al menu la opcion de salir detectando en cual plataforma nos encontramos, agregado el boton y funcionamiento para crear una nueva nota abriendo una pantalla independiente

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const electron = require("electron");
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let newWindow;
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({});
@@ -12,13 +13,43 @@ app.on("ready", () => {
   Menu.setApplicationMenu(mainMenu);
 });
 
+const createNewWindow = () => {
+  newWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add new todo",
+  });
+};
+
 const menuTemplate = [
   {
     label: "File",
     submenu: [
       {
         label: "New todo",
+        click() {
+          createNewWindow();
+        },
+      },
+      {
+        label: "Quit",
+        // accelerator: "Ctrl+Q", // NOTE: Atajo
+        accelerator: (() => {
+          if (process.platform === "darwin") {
+            return "Command+Q";
+          } else {
+            return "Ctrl+Q";
+          }
+        })(),
+        click() {
+          app.quit(); // NOTE: Salir
+        },
       },
     ],
   },
 ];
+
+// En MAC agrega un nuevo elemento al inicio del menu
+if (process.platform === "darwin") {
+  menuTemplate.unshift({});
+}
